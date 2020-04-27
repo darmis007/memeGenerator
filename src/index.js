57aea3f09b4f6600e7d20604c0bc7b96aeabff14
darmis007 <51883443+darmis007@users.noreply.github.com>
2020-04-27 15:20:36 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Header from './components/header'
-import TodoItem from './components/todo'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
